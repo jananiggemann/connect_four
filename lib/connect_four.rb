7abf80cf68853495b0e0e0235b0f20e42a44a816
Jana <jana.niggemann@outlook.de>
2021-08-15 19:38:03 +0200

    Simplify Code

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -1,9 +1,9 @@
 module ConnectFour
-
   # Starts new game by calling the method "moveMaker" up to 64 times 
   def self.run
-    64.times do NewGame.moveMaker end
-    true
+    4.times do NewGame.moveMaker end
+    print "\n      No winner! Game over.\n\n"
+    exit
   end
 
 
@@ -27,39 +27,28 @@ module ConnectFour
   #             . . . . . . . .
   #
   class Gameboard
-
     # Creates initial game board array
     def create_gameboard(x, y, default)
-
       gameboard = Array.new(x)
-    
-      for i in 0...x
-          gameboard[i] = Array.new(y, default)
-      end
+      for i in 0...x do gameboard[i] = Array.new(y, default) end
       return gameboard
-    
     end
 
-    # Prints current state of the game board
+    # Prints current state of the game board, the old one will be replaced by the 
+    # updated game board.
     def print_gameboard(gameboard)
-
       # Clears terminal screen after each move so that in case of any changes
       # on the game board.
-      # The old one will be replaced by the updated game board.
       system "cls"
       system "clear"
 
       print ("\n    Exit game with key 'x'\n\n\n       1 2 3 4 5 6 7 8\n       ")
       for line in gameboard
-        for field in line
-          printf "%s ", field
-        end
+        for field in line do printf "%s ", field end
         print "\n       "
       end
       puts ""
-
     end
-
   end
 
 
@@ -71,11 +60,10 @@ module ConnectFour
   #      winner is announced
   class WinnerDetection
     WinnerGameboard = Gameboard.new
-
     # Method prints last move and which player has won, then exits the game.
     def winner(lastMove, xo)
       WinnerGameboard.print_gameboard(lastMove)
-      print "\n      Player ", xo, " is the winner!\n\n"
+      print "\n    Player ", xo, " is the winner!\n\n"
       exit
     end
 
@@ -93,7 +81,6 @@ module ConnectFour
     # In that case, the "left side" while loop is executed up to 2 times, in case 
     # there are 2 fields of the same colour on the left side.
     def detectVictory(gameState, l, col, xo)
-
       # checks vertically down
       i=1
       while i<4 && l+i<8 && gameState[l+i][col]== xo do i+=1 end
@@ -133,20 +120,17 @@ module ConnectFour
   #  --> Returns changed game board 
   class Move
     NewWinnerDetection = WinnerDetection.new
-
-    # Searches for next empty spot in column
-    def move(gameboard, column, colour)
+    def move(gameboard, col, xo)
       d=gameboard.length
       d= d.to_i
       
       for i in 1..8 
-        if gameboard[d-i][column] == "."
-          gameboard[gameboard.length-i][column] = colour
-          NewWinnerDetection.detectVictory(gameboard, gameboard.length-i, column, colour)
+        if gameboard[d-i][col] == "."
+          gameboard[gameboard.length-i][col] = xo
+          NewWinnerDetection.detectVictory(gameboard, gameboard.length-i, col, xo)
           return gameboard
-        else
-          i+=1
         end
+        i+=1
       end 
       print "\n      Choose different Column!\n"
       NewGame.moveCounter(-1)
@@ -164,7 +148,6 @@ module ConnectFour
   #      game is exited
   #  --> Calls method "Move" to to implement players move
   class Game
-
     # New object of the class "Gameboard" is created and printed in initial state
     NewGameboard = Gameboard.new
     @@gameboard = NewGameboard.create_gameboard(8, 8, ".")
@@ -175,13 +158,11 @@ module ConnectFour
     # Counts how many moves have been made so its possible to know whose turn it is
     def moveCounter(x)
       @@movecount +=x
-      return @@movecount
     end
     
     # Replaces previous game board with an updated version including the last move
     def updateGameboard(move)
       @@gameboard = move
-      return @@gameboard
     end
 
     # Gets the user input, converts it to integer and checks if its within 1-8.
@@ -202,34 +183,22 @@ module ConnectFour
       end
     end
 
-
     NewMove = Move.new
     # Gets the input from player, decides which players turn it is 
     # and gets the updated game board from class "Move".
     def moveMaker
-      mc = moveCounter(1)
-      colour = if mc.even? then "o" else "x" end  
-      
+      colour = if moveCounter(1).even? then "o" else "x" end  
       print "\n  --> Press key 1-8 to pick a column\n\n      Player ", colour, " > "
 
-      column = inputRange(colour)
-
-      move = NewMove.move(@@gameboard, column, colour)
-      
-      print "\n\n"
+      move = NewMove.move(@@gameboard, inputRange(colour), colour)
       NewGameboard.print_gameboard(move)
-      if @@movecount == 64
-        print "\n      No winner! Game over.\n\n"
-        exit
-      end
       updateGameboard(move)
     end
   end
 
 
   # ---------------------------------------------------------------------------------
-  # New Game:
-  # --> Creates new object of the class "Game"
+  # Creates new object of the class "Game"
   NewGame = Game.new
   
 end
