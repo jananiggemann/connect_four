013bda62ecf1bba2b1dd5a2489319f92654468c6
Jana <jana.niggemann@outlook.de>
2021-08-28 21:47:00 +0200

    Edit and adjust method and variable names

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -1,18 +1,20 @@
 require 'io/console'
 
 module ConnectFour
-  # Starts new game by calling the method "moveMaker" up to 64 times 
+  # Starts new game by calling the method "move_maker" up to 64 times 
   def self.run
     newGame = Game.new
-    while $movecount<64 do newGame.move_maker end
-    Gameboard.print_gameboard($gameboard)
+    while $move_counter<64
+      newGame.move_maker
+    end
+    GameBoard.print_game_board($game_board)
     print "\n\tNo winner! Game over.\n\n"
     exit
   end
 
 
   # ---------------------------------------------------------------------------------
-  # Class Gameboard:
+  # Class GameBoard:
   #  --> Creates multi-dimensional 8x8 game board array with dots as default values
   #  --> Prints current state of the game board
   #      Example:
@@ -30,25 +32,29 @@ module ConnectFour
   #             . . . . . . . .
   #             . . . . . . . .
   #
-  class Gameboard
+  class GameBoard
     # Creates initial game board array
-    def create_gameboard(x, y, default)
-      gameboard = Array.new(x)
-      for i in 0...x do gameboard[i] = Array.new(y, default) end
-      return gameboard
+    def create_game_board(x, y, default)
+      game_board = Array.new(x)
+      for i in 0...x
+        game_board[i] = Array.new(y, default) 
+      end
+      return game_board
     end
 
     # Prints current state of the game board, the old one will be replaced by the 
     # updated game board.
-    def self.print_gameboard(gameboard)
+    def self.print_game_board(game_board)
       # Clears terminal screen after each move so that in case of any changes
       # on the game board.
       system "cls"
       system "clear"
 
       print ("\n\tExit game with key 'x'\n\n\n\t   1 2 3 4 5 6 7 8\n\t   ")
-      for line in gameboard
-        for field in line do printf "%s ", field end
+      for line in game_board
+        for field in line
+          printf "%s ", field
+        end
         print "\n\t   "
       end
       puts ""
@@ -63,10 +69,10 @@ module ConnectFour
   #  --> If thats the case, the final state of the game board is printed and the 
   #      winner is announced
   # Method "winner":
-  #  --> Calls method "detectVictory" and passes the line of the last move as an argument
-  #  --> If method "detectVictory" returns 1, the game board is printed one last time, 
+  #  --> Calls method "victory?" and passes the line of the last move as an argument
+  #  --> If method "victory?" returns 1, the game board is printed one last time, 
   #      the winner is announced and the game ends
-  # Method "detect Victory":
+  # Method "victory?":
   #  --> Gets the line of the last move as an argument
   #  --> Checks all neighboring positions of the last move 
   #  --> If there are fields of the same colour ($x_or_o) on both sides 
@@ -82,40 +88,51 @@ module ConnectFour
   class WinnerDetection
     # Method "winner" prints last move and which player has won, then exits the game.
     def winner(l)
-      if detect_victory(l) == 1
-        Gameboard.print_gameboard($gameboard)
+      if victory?(l) == "winner"
+        GameBoard.print_game_board($game_board)
         print "\n\tPlayer ", $x_or_o, " is the winner!\n\n"
         exit
       end
     end
 
-    def detect_victory(l)
+    def victory?(l)
       # checks vertically down
       i=1
-      while i<4 && l+i<8 && $gameboard[l+i][$col]== $x_or_o do i+=1 end
-      if i==4 then return 1 end
+      while i<4 && l+i<8 && $game_board[l+i][$col]== $x_or_o
+        i+=1
+      end
+      if i==4 then return "winner" end
       
       # checks horizontally right and then left
       i=1
-      while i<4 && $col+i<8 && $gameboard[l][$col+i]== $x_or_o do i+=1 end
+      while i<4 && $col+i<8 && $game_board[l][$col+i]== $x_or_o do i+=1 end
       x=1
-      while i+x<5 && $col-x>-1 && $gameboard[l][$col-x]== $x_or_o do x+=1 end
-      if i+x==5 then return 1 end
+      while i+x<5 && $col-x>-1 && $game_board[l][$col-x]== $x_or_o
+        x+=1 
+      end
+      if i+x==5 then return "winner" end
 
       # checks diagonally down left and then up right
       i=1
-      while i<4 && $col-i>-1 && l+i<8 && $gameboard[l+i][$col-i]== $x_or_o do i+=1 end
+      while i<4 && $col-i>-1 && l+i<8 && $game_board[l+i][$col-i]== $x_or_o
+        i+=1
+      end
       x=1
-      while i+x<5 && $col+x<8 && l-x>-1 && $gameboard[l-x][$col+x]== $x_or_o do x+=1 end
-      if i+x==5 then return 1 end
+      while i+x<5 && $col+x<8 && l-x>-1 && $game_board[l-x][$col+x]== $x_or_o
+        x+=1
+      end
+      if i+x==5 then return "winner" end
 
       # checks diagonally down right and then up left
       i=1
-      while i<4 && $col+i<8 && l+i<8 && $gameboard[l+i][$col+i]== $x_or_o do i+=1 end
+      while i<4 && $col+i<8 && l+i<8 && $game_board[l+i][$col+i]== $x_or_o
+        i+=1
+      end
       x=1
-      while i+x<5 && $col-x>-1 && l-x>-1 && $gameboard[l-x][$col-x]== $x_or_o do x+=1 end
-      if i+x==5 then return 1 end
-      return 2
+      while i+x<5 && $col-x>-1 && l-x>-1 && $game_board[l-x][$col-x]== $x_or_o
+        x+=1 
+      end
+      if i+x==5 then return "winner" end
     end
   end
 
@@ -129,47 +146,46 @@ module ConnectFour
   #  --> Returns changed game board 
   #  --> If there is no empty space in the chosen column ($col), 0 is returned 
   class Move
-    def self.move
+    def self.check_move
       newWinnerDetection = WinnerDetection.new
-      d=$gameboard.length
+      d=$game_board.length
       d= d.to_i
       
       for i in 1..8 
-        if $gameboard[d-i][$col] == "."
-          $gameboard[d-i][$col] = $x_or_o
+        if $game_board[d-i][$col] == "."
+          $game_board[d-i][$col] = $x_or_o
           newWinnerDetection.winner(d-i)
           return 
         end
         i+=1
       end 
-      return 0
+      return "column complete"
     end
   end
 
 
   # ---------------------------------------------------------------------------------
   # Class game:
-  #  --> Creates new object of the class "Gameboard" and prints it, so that the 
+  #  --> Creates new object of the class "GameBoard" and prints it, so that the 
   #      initial state of the game is displayed
   #  --> Creates new object of the class "Move"
   #  --> Gets the user input and checks if its between 1 and 8, in case its "x" the 
   #      game is exited
-  #  --> Calls method "Move" to implement players move
+  #  --> Calls method "check_move" to implement players move
   class Game
     # New object of the class "Gameboard" is created and printed in initial state
-    $gameboard = Gameboard.new.create_gameboard(8, 8, ".")
+    $game_board = GameBoard.new.create_game_board(8, 8, ".")
     # Counts how many moves have been made so its possible to know whose turn it is
-    $movecount = 0
+    $move_counter = 0
     # Gets the user input, converts it to integer and checks if its within 1-8.
     # If its not within that range, the method calls itself again.
     def input_range
-      a = STDIN.getch
-      print a
+      input = STDIN.getch
+      print input
       sleep(0.17) #to see player input on console 
-      if a.to_i>0 && a.to_i<9
-        $col = a.to_i-1
-        return 
-      elsif a == "x"
+      if input.to_i>0 && input.to_i<9
+        $col = input.to_i-1
+      elsif input == "x"
         print "\n\n\n"
         exit
       else
@@ -183,14 +199,14 @@ module ConnectFour
     # In case there is no empty space in the chosen column the player can choose
     # a different one.
     def move_maker
-      Gameboard.print_gameboard($gameboard)
-      $movecount+=1
-      $x_or_o = if $movecount.even? then "o" else "x" end  
+      GameBoard.print_game_board($game_board)
+      $move_counter+=1
+      $x_or_o = if $move_counter.even? then "o" else "x" end  
       print "\n    --> Press key 1-8 to pick a column\n\n\tPlayer ", $x_or_o, " > "
       input_range()
-      if Move.move == 0
-        print "\n\n\tChoose different Column!"
-        $movecount-=1
+      if Move.check_move == "column complete"
+        print "\n\n\tChoose different column!"
+        $move_counter-=1
         sleep(1)
       end
     end
